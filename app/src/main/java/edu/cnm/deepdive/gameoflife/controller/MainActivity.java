30740582f9804cda1d3f4919fb90e452cd21886f
Nick Bennett <nick@nickbenn.com>
2021-01-13 08:53:52 -0700

    Initial commit.

---
+++ app/src/main/java/edu/cnm/deepdive/gameoflife/controller/MainActivity.java
@@ -0,0 +1,81 @@
+/*
+ *  Copyright 2021 CNM Ingenuity, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package edu.cnm.deepdive.gameoflife.controller;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.gameoflife.R;
+import edu.cnm.deepdive.gameoflife.viewmodel.MainViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+  private MainViewModel viewModel;
+  private boolean running;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    getLifecycle().addObserver(viewModel);
+    viewModel.getRunning().observe(this, (running) -> {
+      if (running != this.running) {
+        this.running = running;
+        invalidateOptionsMenu();
+      }
+    });
+    // TODO Setup bindings.
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.actions, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    menu.findItem(R.id.run).setVisible(!running);
+    menu.findItem(R.id.pause).setVisible(running);
+    menu.findItem(R.id.reset).setVisible(!running);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    boolean handled = true;
+    switch (item.getItemId()) {
+      case R.id.run:
+        viewModel.start();
+        break;
+      case R.id.pause:
+        viewModel.stop();
+        break;
+      case R.id.reset:
+        viewModel.reset();
+        break;
+      default:
+        handled = super.onOptionsItemSelected(item);
+        break;
+    }
+    return handled;
+  }
+
+}
